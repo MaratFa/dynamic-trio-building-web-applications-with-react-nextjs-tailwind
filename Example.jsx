8b8d40bbf229df711816b36f74b01f6aa97231cb
Marat <80181316+MaratFa@users.noreply.github.com>
2023-08-16 07:35:33 +0300

    Create Example.jsx

---
+++ Example.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function Example() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [email, setEmail] = useState("");
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={handleNameChange}
+      />
+      <input
+        type="number"
+        placeholder="Age"
+        value={age}
+        onChange={handleAgeChange}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <p>
+        {name} is {age} years old and their email is {email}.
+      </p>
+    </div>
+  );
+}
+export default Example;
